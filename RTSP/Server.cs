58d75ef046c3fd8d6e87e1f267398576946b4969
NakanoMiku13 <danielulrichtamayo@gmail.com>
2025-08-06 09:59:01 -0600

    Fixing GC error 'Action not supported on this platform'

--- RTSP/Server.cs
+++ RTSP/Server.cs
@@ -249,7 +249,7 @@ public class Server : IDisposable
     public void Stop() => Dispose();
     public bool Start()
     {
-        GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+        //GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
         if (_enabled)
         {
             _backService.FrameReceived += OnBackFrameAvailable;
